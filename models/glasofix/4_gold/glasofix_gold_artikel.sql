a44a5c7491bc4ecc1a8d06e4ed2e5af609438d7e
Henrik Giesler <henrik.giesler@wencke-gruppe.local>
2026-06-25 16:59:50 +0200

    artikelstamm glasofix

--- models/glasofix/4_gold/glasofix_gold_artikel.sql
+++ models/glasofix/4_gold/glasofix_gold_artikel.sql
@@ -23,6 +23,13 @@ nebenwarengruppe as (
 lieferant as (
     select *
     from {{ ref('glasofix_silver_lieferant') }}
+),
+
+tos as (
+select distinct         
+        ar_text as art_artikelnummer
+    from {{ ref('wencke_bronze_tos_artikel_attribut') }}
+    where ar_art = 1389
 )
 
 select 
@@ -55,15 +62,21 @@ select
     coalesce(lieferant.adr_standardlieferantname, 'keine Bezeichnung') 
         as art_lieferantbezeichnung,
 
-    CASE when artikel.art_divers_flag = 'J' then 'Nur diverse Produkten'
-    ELSE 'Ohne diversen Produkten'
-    END as art_divers_flag,
+    case 
+        when artikel.art_divers_flag = 'J' then 'Nur diverse Produkten'
+        else 'Ohne diversen Produkten'
+    end as art_divers_flag,
 
     artikel.art_ek_netto,
     artikel.art_lagereinheit,
     artikel.art_bezeichnung_2,
     artikel.art_bezeichnung_3,
-    artikel.art_bezeichnung_4
+    artikel.art_bezeichnung_4,
+
+    case
+        when tos.art_artikelnummer is not null then 'J'
+        else 'N'
+    end as art_tos_verfuegbar
 
 from artikel
 
@@ -75,5 +88,8 @@ left join nebenwarengruppe
 
 left join lieferant 
     on artikel.art_standardlieferantnummer = lieferant.art_standardlieferantnummer
+
+left join tos
+    on tos.art_artikelnummer = artikel.art_artikelnummer
 
 order by artikel.art_artikelnummer
