778337361fd7aa051ea7348302f300acf3330db0
fnfondatec <fn@fondatec.de>
2026-07-06 11:28:48 +0200

    art_bez bei lloyd und glasofix geaddet

--- models/glasofix/1_bronze/glasofix_bronze_adresse.sql
+++ models/glasofix/1_bronze/glasofix_bronze_adresse.sql
@@ -5,6 +5,7 @@ with source_data as (
     select
         adr_2_8                              as adr_adressnummer,
         adr_20_30                            as adr_name,
+        adr_50_30                            as adr_name_2,
         adr_80_30                            as adr_adresse,
         adr_110_10                           as adr_plz,
         adr_120_30                           as adr_stadt,
@@ -21,8 +22,20 @@ with source_data as (
         adr_822_1                            as adr_oe_3,
         adr_823_1                            as adr_oe_4,
         adr_875_8                            as adr_zentrale_GVS_nummer,
-        adr_1219_2                           as adr_adresstyp
-    from {{ source('raw', 'm39adr') }} 
+        adr_1024_30                          as adr_name_3,
+        adr_1219_2                           as adr_adresstyp,
+        adr_1054_1                           as adr_servicehandbuch_vorhanden,
+        adr_1055_1                           as adr_servicehandbuch_jahr,
+        adr_1056_1                           as adr_servicehandbuch_monat,
+        adr_1057_1                           as adr_dosiertechnik,
+        adr_1058_1                           as adr_serviceintervall_anzahl_jaehrlich,
+        adr_1059_1                           as adr_schulung,
+        adr_1060_1                           as adr_letzte_schulung_jahr,
+        adr_1061_1                           as adr_letzte_schulung_monat,
+        adr_1062_1                           as adr_haende_hygieneplan_vorhanden,
+        adr_1063_1                           as adr_gefahrstoffverzeichnis
+
+    from {{ source('raw', 'm39adr') }}
 
 )
 
