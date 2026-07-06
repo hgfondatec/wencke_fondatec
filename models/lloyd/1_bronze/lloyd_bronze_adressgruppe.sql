01f9e1d2634c41d752c70e79deab78d016dae627
Henrik Giesler <henrik.giesler@wencke-gruppe.local>
2026-07-06 11:32:28 +0200

    add adjust lloyd adressgruppe

--- models/lloyd/1_bronze/lloyd_bronze_adressgruppe.sql
+++ models/lloyd/1_bronze/lloyd_bronze_adressgruppe.sql
@@ -3,7 +3,7 @@
 with source_data as (
 
     select
-        distinct TRUNC(agp_1_2)            as adrgruppe_id,
+        distinct agp_1_2                   as adrgruppe_id,
         agp_3_30                           as adrgruppe_name
 
     from {{ source('raw', 'm32adrgrp') }}
