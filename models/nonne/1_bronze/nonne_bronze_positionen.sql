9a3ae9053a2a7f390625582caf1e27dd4a2c1759
Henrik Giesler <henrik.giesler@wencke-gruppe.local>
2026-07-13 17:10:51 +0200

    adjust rekla wencke

--- models/nonne/1_bronze/nonne_bronze_positionen.sql
+++ models/nonne/1_bronze/nonne_bronze_positionen.sql
@@ -30,7 +30,7 @@ with source_data as (
         pos_1766_7         as pos_umsatz_bonus_endgueltig
         
 
-    from {{ source('raw', 'm36pos_test_v2') }}
+    from {{ source('raw', 'm36pos_test_v5') }}
 
 )
 
