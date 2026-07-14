4fb1616e5c3389187c8c99b8d029a2a96cabcd0a
fnfondatec <fn@fondatec.de>
2026-07-14 15:01:37 +0200

    change amtch value

--- macros/match_value.sql
+++ macros/match_value.sql
@@ -1,14 +1,13 @@
 {% macro match_value(field) %}
 
 (
-    select value
+    select string_agg(value, '/' order by value)
     from (
         select
             value,
             count(*) as score,
             length(value) as len
         from (
-
             select nullif(trim(t39.{{ field }}::text), '') as value
             union all
             select nullif(trim(t32.{{ field }}::text), '')
@@ -16,13 +15,29 @@
             select nullif(trim(t42.{{ field }}::text), '')
             union all
             select nullif(trim(t36.{{ field }}::text), '')
-
         ) s
         where value is not null
         group by value
     ) x
-    order by score desc, len asc
-    limit 1
+    where score = (
+        select max(score)
+        from (
+            select
+                value,
+                count(*) as score
+            from (
+                select nullif(trim(t39.{{ field }}::text), '') as value
+                union all
+                select nullif(trim(t32.{{ field }}::text), '')
+                union all
+                select nullif(trim(t42.{{ field }}::text), '')
+                union all
+                select nullif(trim(t36.{{ field }}::text), '')
+            ) s
+            where value is not null
+            group by value
+        ) m
+    )
 )
 
 {% endmacro %}
