b72695a63aa1e67ca0c29c722364d121e8bd3bff
Henrik Giesler <henrik.giesler@wencke-gruppe.local>
2026-07-16 10:12:56 +0200

    adjust wencke gold adress

--- models/wencke/4_gold/wencke_gold_rekla_facts.sql
+++ models/wencke/4_gold/wencke_gold_rekla_facts.sql
@@ -7,16 +7,17 @@
 
 with final as (
 
-    select vms.*, 42 as mandant_id from {{ ref('vms_gold_rekla_facts') }} vms
+    select concat(rekla_adress_nr,'_32') as adress_key, lloyd.*, 32 as mandant_id from {{ ref('lloyd_gold_rekla_facts') }} lloyd
     union
-    select nonne.*, 36 as manadant_id from {{ ref('nonne_gold_rekla_facts') }} nonne
+    select concat(rekla_adress_nr,'_36') as adress_key, nonne.*, 36 as mandant_id from {{ ref('nonne_gold_rekla_facts') }} nonne
     union
-    select lloyd.*, 32 as manadant_id from {{ ref('lloyd_gold_rekla_facts') }} lloyd
+    select concat(rekla_adress_nr,'_39') as adress_key, glasofix.*, 39 as mandant_id from {{ ref('glasofix_gold_rekla_facts') }} glasofix
     union
-    select glasofix.*, 39 as manadant_id from {{ ref('glasofix_gold_rekla_facts') }} glasofix
+    select concat(rekla_adress_nr,'_42') as adress_key, vms.*, 42 as mandant_id from {{ ref('vms_gold_rekla_facts') }} vms
 
 )
 
 select
     *
 from final
+order by adress_key
