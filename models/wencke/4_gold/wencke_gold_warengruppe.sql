90c807e5baf9541aebe3fea4b1c76499d81edaa0
fnfondatec <fn@fondatec.de>
2026-08-03 11:23:15 +0200

    push wencke gold warengruppe

--- models/wencke/4_gold/wencke_gold_warengruppe.sql
+++ models/wencke/4_gold/wencke_gold_warengruppe.sql
@@ -5,44 +5,7 @@
     )
 }}
 
-with warengruppen as (
-
-    select
-        art_hauptwarengruppe_nummer,
-        art_nebenwarengruppe_nummer
-    from {{ ref('glasofix_gold_artikel') }}
-
-    union
-
-    select
-        art_hauptwarengruppe_nummer,
-        art_nebenwarengruppe_nummer
-    from {{ ref('lloyd_gold_artikel') }}
-
-    union
-
-    select
-        art_hauptwarengruppe_nummer,
-        art_nebenwarengruppe_nummer
-    from {{ ref('vms_gold_artikel') }}
-
-    union
-
-    select
-        art_hauptwarengruppe_nummer,
-        art_nebenwarengruppe_nummer
-    from {{ ref('nonne_gold_artikel_v2') }}
-
-    union
-
-    select
-        art_hauptwarengruppe_nummer,
-        art_nebenwarengruppe_nummer
-    from {{ ref('kernreich_gold_artikel_v2') }}
-
-),
-
-t39 as (
+with t39 as (
 
     select distinct
         art_hauptwarengruppe_nummer,
@@ -105,19 +68,97 @@ t38 as (
         art_nebenwarengruppebezeichnung
     from {{ ref('kernreich_gold_artikel_v2') }}
 
+),
+
+hauptwarengruppe_ids as (
+
+    select art_hauptwarengruppe_nummer from t39
+    union
+    select art_hauptwarengruppe_nummer from t32
+    union
+    select art_hauptwarengruppe_nummer from t42
+    union
+    select art_hauptwarengruppe_nummer from t36
+    union
+    select art_hauptwarengruppe_nummer from t38
+
+),
+
+gold_hauptwarengruppe as (
+
+    select
+
+        base.art_hauptwarengruppe_nummer,
+
+        {{ golden_value('art_hauptwarengruppe') }}
+            as art_hauptwarengruppe,
+
+        {{ golden_value('art_hauptwarenbezeichnung') }}
+            as art_hauptwarenbezeichnung
+
+    from hauptwarengruppe_ids base
+
+    left join t39
+        on base.art_hauptwarengruppe_nummer = t39.art_hauptwarengruppe_nummer
+
+    left join t32
+        on base.art_hauptwarengruppe_nummer = t32.art_hauptwarengruppe_nummer
+
+    left join t42
+        on base.art_hauptwarengruppe_nummer = t42.art_hauptwarengruppe_nummer
+
+    left join t36
+        on base.art_hauptwarengruppe_nummer = t36.art_hauptwarengruppe_nummer
+
+    left join t38
+        on base.art_hauptwarengruppe_nummer = t38.art_hauptwarengruppe_nummer
+
+),
+
+warengruppe_ids as (
+
+    select
+        art_hauptwarengruppe_nummer,
+        art_nebenwarengruppe_nummer
+    from t39
+
+    union
+
+    select
+        art_hauptwarengruppe_nummer,
+        art_nebenwarengruppe_nummer
+    from t32
+
+    union
+
+    select
+        art_hauptwarengruppe_nummer,
+        art_nebenwarengruppe_nummer
+    from t42
+
+    union
+
+    select
+        art_hauptwarengruppe_nummer,
+        art_nebenwarengruppe_nummer
+    from t36
+
+    union
+
+    select
+        art_hauptwarengruppe_nummer,
+        art_nebenwarengruppe_nummer
+    from t38
+
 )
 
 select
 
-    base.art_hauptwarengruppe_nummer,
+    wg.art_hauptwarengruppe_nummer,
+    hwg.art_hauptwarengruppe,
+    hwg.art_hauptwarenbezeichnung,
 
-    {{ golden_value('art_hauptwarengruppe') }}
-        as art_hauptwarengruppe,
-
-    {{ golden_value('art_hauptwarenbezeichnung') }}
-        as art_hauptwarenbezeichnung,
-
-    base.art_nebenwarengruppe_nummer,
+    wg.art_nebenwarengruppe_nummer,
 
     {{ golden_value('art_nebenwarengruppe') }}
         as art_nebenwarengruppe,
@@ -125,24 +166,37 @@ select
     {{ golden_value('art_nebenwarengruppebezeichnung') }}
         as art_nebenwarengruppebezeichnung
 
-from warengruppen base
+from warengruppe_ids wg
+
+left join gold_hauptwarengruppe hwg
+    on wg.art_hauptwarengruppe_nummer = hwg.art_hauptwarengruppe_nummer
 
 left join t39
-    on base.art_hauptwarengruppe_nummer = t39.art_hauptwarengruppe_nummer
-   and base.art_nebenwarengruppe_nummer = t39.art_nebenwarengruppe_nummer
+    on wg.art_hauptwarengruppe_nummer = t39.art_hauptwarengruppe_nummer
+   and wg.art_nebenwarengruppe_nummer = t39.art_nebenwarengruppe_nummer
 
 left join t32
-    on base.art_hauptwarengruppe_nummer = t32.art_hauptwarengruppe_nummer
-   and base.art_nebenwarengruppe_nummer = t32.art_nebenwarengruppe_nummer
+    on wg.art_hauptwarengruppe_nummer = t32.art_hauptwarengruppe_nummer
+   and wg.art_nebenwarengruppe_nummer = t32.art_nebenwarengruppe_nummer
 
 left join t42
-    on base.art_hauptwarengruppe_nummer = t42.art_hauptwarengruppe_nummer
-   and base.art_nebenwarengruppe_nummer = t42.art_nebenwarengruppe_nummer
+    on wg.art_hauptwarengruppe_nummer = t42.art_hauptwarengruppe_nummer
+   and wg.art_nebenwarengruppe_nummer = t42.art_nebenwarengruppe_nummer
 
 left join t36
-    on base.art_hauptwarengruppe_nummer = t36.art_hauptwarengruppe_nummer
-   and base.art_nebenwarengruppe_nummer = t36.art_nebenwarengruppe_nummer
+    on wg.art_hauptwarengruppe_nummer = t36.art_hauptwarengruppe_nummer
+   and wg.art_nebenwarengruppe_nummer = t36.art_nebenwarengruppe_nummer
 
 left join t38
-    on base.art_hauptwarengruppe_nummer = t38.art_hauptwarengruppe_nummer
-   and base.art_nebenwarengruppe_nummer = t38.art_nebenwarengruppe_nummer
+    on wg.art_hauptwarengruppe_nummer = t38.art_hauptwarengruppe_nummer
+   and wg.art_nebenwarengruppe_nummer = t38.art_nebenwarengruppe_nummer
+
+group by
+    wg.art_hauptwarengruppe_nummer,
+    hwg.art_hauptwarengruppe,
+    hwg.art_hauptwarenbezeichnung,
+    wg.art_nebenwarengruppe_nummer
+
+order by
+    wg.art_hauptwarengruppe_nummer,
+    wg.art_nebenwarengruppe_nummer
