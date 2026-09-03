8c09fc0759a4caf2dd60b4491fec3a9a96e5ded0
fnfondatec <fn@fondatec.de>
2026-09-03 13:13:38 +0200

    push kernreich wgs und new golden value

--- macros/golden_value_wencke.sql
+++ macros/golden_value_wencke.sql
@@ -4,17 +4,16 @@
     SELECT value
     FROM (
         SELECT
-            NULLIF(TRIM(s.{{ field }}::text), '') AS value,
+            s.{{ field }}::text AS value,
             COUNT(*) AS score
         FROM {{ ref('silver_wencke_artikel') }} s
         WHERE s.art_artikelnummer = base.art_artikelnummer
-          AND NULLIF(TRIM(s.{{ field }}::text), '') IS NOT NULL
-        GROUP BY
-            NULLIF(TRIM(s.{{ field }}::text), '')
+          AND s.{{ field }} IS NOT NULL
+        GROUP BY s.{{ field }}::text
     ) x
     ORDER BY
         score DESC,
-        LENGTH(REGEXP_REPLACE(value, '\s+', '', 'g')) DESC,
+        LENGTH(value) DESC,
         value ASC
     LIMIT 1
 )
