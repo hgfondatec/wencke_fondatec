253faa0147d9264f3c8578d492ed61f1f4075e79
fnfondatec <fn@fondatec.de>
2026-09-08 16:48:52 +0200

    change golde wenkce artikel leiferant

--- models/reporting_wencke/gold/gold_wencke_artikel_lieferant.sql
+++ models/reporting_wencke/gold/gold_wencke_artikel_lieferant.sql
@@ -16,6 +16,17 @@ WITH artikel_lieferant AS (
         topserv_lieferanten_nr,
         adr_zentral_kunden_nr,
 
+        NULLIF(
+            TRIM(
+                REGEXP_REPLACE(
+                    art_lieferantbezeichnung,
+                    '^\s*[0-9]+\s*-\s*',
+                    ''
+                )
+            ),
+            ''
+        ) AS lieferantenbezeichnung_bereinigt,
+
         CONCAT(
             COALESCE(art_artikelnummer::text, ''),
             '_',
@@ -24,7 +35,58 @@ WITH artikel_lieferant AS (
 
     FROM {{ ref('silver_wencke_artikel_lieferant') }}
 
+),
+
+harmonisierung_basis AS (
+
+    SELECT
+        adr_zentral_kunden_nr,
+        lieferantenbezeichnung_bereinigt,
+        COUNT(*) AS anzahl
+
+    FROM artikel_lieferant
+
+    WHERE adr_zentral_kunden_nr IS NOT NULL
+      AND lieferantenbezeichnung_bereinigt IS NOT NULL
+
+    GROUP BY
+        adr_zentral_kunden_nr,
+        lieferantenbezeichnung_bereinigt
+
+),
+
+harmonisierung AS (
+
+    SELECT
+        adr_zentral_kunden_nr,
+        lieferantenbezeichnung_bereinigt
+            AS art_lieferantbezeichnung_harmonisiert
+
+    FROM (
+
+        SELECT
+            *,
+            ROW_NUMBER() OVER (
+                PARTITION BY adr_zentral_kunden_nr
+                ORDER BY
+                    anzahl DESC,
+                    LENGTH(lieferantenbezeichnung_bereinigt) ASC,
+                    lieferantenbezeichnung_bereinigt ASC
+            ) AS rn
+
+        FROM harmonisierung_basis
+
+    ) x
+
+    WHERE rn = 1
+
 )
 
-SELECT *
-FROM artikel_lieferant
+SELECT
+    a.*,
+    h.art_lieferantbezeichnung_harmonisiert
+
+FROM artikel_lieferant AS a
+
+LEFT JOIN harmonisierung AS h
+    ON a.adr_zentral_kunden_nr = h.adr_zentral_kunden_nr
