c424cea9bbbb831dcf0a9eee619d4ba70eabc1b9
Finn Niehues <fn@fondatec.de>
2026-09-10 14:54:04 +0200

    commit artiel kategorie

---
+++ models/reporting_wencke/bronze/bronze_wencke_artikel_kategorien.sql
@@ -0,0 +1,34 @@
+{{
+    config(
+        materialized = 'table',
+        tags = ['artikel']
+    )
+}}
+
+SELECT
+    kategorie_nr,
+    mandant,
+    wencke_id,
+    is_hauptkategorie,
+    kat_bezeichnung,
+    kat_suchindex_1,
+    kat_suchindex_2,
+    kat_suchindex_3,
+    kat_suchindex_4,
+    kat_suchindex_5,
+    kat_positionserfassung,
+    kat_einzelkalkulation,
+    kat_anzahl_positionen,
+    kat_erfassungstabelle,
+    kat_aenderungsflag_dbk_dbp,
+    kat_wfl_script,
+    kat_wfl_verarbeitung,
+    kat_wechsel_kartenummer,
+    kat_logo_hauptkategorie,
+    kat_werbebild_a4,
+    kat_erfasst_von,
+    kat_geaendert_von
+
+FROM {{ source('raw', 'wencke_lv_artikel_kategorien') }}
+
+WHERE kategorie_nr IS NOT NULL
---
+++ models/reporting_wencke/gold/gold_wencke_artikel_kategorien.sql
@@ -0,0 +1,33 @@
+{{
+    config(
+        materialized = 'table',
+        tags = ['artikel']
+    )
+}}
+
+SELECT DISTINCT
+
+    CONCAT(
+        mandant,
+        '_',
+        nebenkategorie_nummer
+    ) AS kategorie_key,
+
+    mandant,
+
+    hauptkategorie_nummer,
+    hauptkategorie_name,
+    hauptkategorie_harmonisiert,
+
+    nebenkategorie_nummer,
+    nebenkategorie_name,
+    nebenkategorie_harmonisiert
+
+FROM {{ ref('silver_wencke_kategorien') }}
+
+WHERE nebenkategorie_nummer IS NOT NULL
+
+ORDER BY
+    mandant,
+    hauptkategorie_nummer,
+    nebenkategorie_nummer
